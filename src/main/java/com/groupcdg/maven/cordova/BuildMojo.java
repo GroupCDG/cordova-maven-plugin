3a10b2c4b4ffb8538f2500970d11f34a92917ad7
simonsparks <simon@sparks.uk.net>
2014-08-29 11:50:28 +0100

    Updated license documentation.

--- src/main/java/com/groupcdg/maven/cordova/BuildMojo.java
+++ src/main/java/com/groupcdg/maven/cordova/BuildMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 Computing Distribution Group Ltd.
+ * Copyright 2014 Computing Distribution Group Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
